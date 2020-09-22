e6a9f8cd15898a87294039d0523aea12a7150811
Matthieu Mota <matthieumota@gmail.com>
2020-09-22 14:37:01 +0200

    User security

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('fiorella@boxydev.com');
+        $user->setPassword($this->encoder->encodePassword($user, 'daddy'));
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
